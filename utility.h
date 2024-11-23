88e70af0f25b01828d6463206063a26e9bf6b1ec
snaazy <yassinefekih78@outlook.fr>
2024-11-23 13:43:38 +0100

    add utility function

---
+++ utility.h
@@ -0,0 +1,10 @@
+#ifndef UTILITY_H
+#define UTILITY_H
+
+#include <set>
+#include <string>
+
+// format a set of arguments into a string
+std::string formatExtension(const std::set<std::string>& extension);
+
+#endif 
